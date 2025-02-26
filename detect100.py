a1051f46ccf35c679d604ba4a3b12a52823f09c7
sannan <sannan.abbasi@edgeops.ai>
2025-02-26 20:55:26 +0500

    latest changes

--- detect100.py
+++ detect100.py
@@ -4,14 +4,11 @@ import os
 import pandas as pd
 from ultralytics import YOLO
 
-# Load YOLO model
-yolo_model = YOLO("last (8).pt")  # Ensure this model is trained for both detection and classification
-
-# Device configuration
+yolo_model = YOLO("model.pt")  
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 yolo_model.to(device)
 
-def detect_and_classify_yolo(directory_path, output_csv="results.csv"):
+def detect_and_classify_yolo(directory_path, output_csv="results_2.csv"):
     results_list = []
     
     for filename in os.listdir(directory_path):
@@ -22,23 +19,16 @@ def detect_and_classify_yolo(directory_path, output_csv="results.csv"):
                 print(f"Error: Could not read {filename}!")
                 continue
             
-            # Resize image to 128x128
-            image = cv2.resize(image, (128, 128))
-            
-            # Perform YOLO inference with enforced image size
-            results = yolo_model.predict(image, imgsz=128)
+            results = yolo_model.predict(image, imgsz=128,conf=0.35)
             
             for result in results:
                 for box, cls in zip(result.boxes.xyxy.cpu().numpy(), result.boxes.cls.cpu().numpy()):
                     x1, y1, x2, y2 = map(int, box[:4])
-                    gender = "Female" if cls == 0 else "Male"  # Adjust based on YOLO class mapping
+                    gender = "Female" if cls == 0 else "Male"  
                     results_list.append([filename, gender])
                     print(f"{filename}: {gender}")
             
-    # Save results to CSV
     df = pd.DataFrame(results_list, columns=["Image Name", "Predicted Gender"])
     df.to_csv(output_csv, index=False)
     print(f"Results saved to {output_csv}")
-
-# Example Usage
-detect_and_classify_yolo("testing")
+detect_and_classify_yolo("testing2")
